ce24607502f359dbafb020aca01a3b86cbfde4d9
aadilMohamed <72676882+aadilMohamed@users.noreply.github.com>
2020-11-18 16:12:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,23 +16,24 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	package = createSprite(0, 80, 10,10);
+	package.addImage(packageIMG)
+	package.scale=0.25
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	helicopter = createSprite(width/2, 200, 10,10);
+	helicopter.addImage(helicopterIMG)
+	helicopter.scale=0.6
+
+	groundSprite = createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor = color(255)
 
 
 	engine = Engine.create();
 	world = engine.world;
-    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
-	packageBody.x = packageSprite.x
-    packageBody.y = packageSprite.y
+    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:false});
+	packageBody.x = package.x
+    packageBody.y = package.y
 	World.add(world, packageBody);
 	
 
@@ -49,18 +50,14 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  package.x = packageBody.position.x 
+  package.y = packageBody.position.y 
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
     packageBody.setStatic = false;
   }
 }
-
-
-
